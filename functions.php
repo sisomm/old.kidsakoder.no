60caa033d81e1bdcbc5b7a5a340cd0896b0ca5a4
Benedicte Raae <raae@bgraphic.no>
2013-10-08 15:43:23 +0200

    Advanced custom fields on homepage blocks

--- functions.php
+++ functions.php
@@ -1,5 +1,79 @@
-<?php 
+<?php
 
-/* Insert custom functions here */
+/**
+ *  Use Advanced Custom Field as a part of theme
+ *
+ */
 
-?>
+define( 'ACF_LITE', true );
+include_once('advanced-custom-fields/acf.php');
+
+/**
+ *  Install Add-ons
+ *
+ *  The following code will include all 4 premium Add-Ons in your theme.
+ *  Please do not attempt to include a file which does not exist. This will produce an error.
+ *
+ *  The following code assumes you have a folder 'add-ons' inside your theme.
+ *
+ *  IMPORTANT
+ *  Add-ons may be included in a premium theme/plugin as outlined in the terms and conditions.
+ *  For more information, please read:
+ *  - http://www.advancedcustomfields.com/terms-conditions/
+ *  - http://www.advancedcustomfields.com/resources/getting-started/including-lite-mode-in-a-plugin-theme/
+ */
+
+// Add-ons
+// include_once('add-ons/acf-repeater/acf-repeater.php');
+// include_once('add-ons/acf-gallery/acf-gallery.php');
+// include_once('add-ons/acf-flexible-content/acf-flexible-content.php');
+// include_once( 'add-ons/acf-options-page/acf-options-page.php' );
+
+/**
+ *  Register Field Groups
+ *
+ *  The register_field_group function accepts 1 array which holds the relevant data to register a field group
+ *  You may edit the array as you see fit. However, this may result in errors if the array is not compatible with ACF
+ */
+
+  if(function_exists("register_field_group"))
+  {
+
+    register_field_group(array (
+      'id' => 'acf_feature-block-extention',
+      'title' => 'Feature block extention',
+      'fields' => array (
+        array (
+          'key' => 'field_5232c844cf203',
+          'label' => 'Link',
+          'name' => 'link',
+          'type' => 'text',
+          'instructions' => 'Where should this block lead to? Both relative and absolute urls are allowed.',
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'formatting' => 'none',
+          'maxlength' => '',
+        ),
+      ),
+      'location' => array (
+        array (
+          array (
+            'param' => 'post_type',
+            'operator' => '==',
+            'value' => 'st_hpfeatures',
+            'order_no' => 0,
+            'group_no' => 0,
+          ),
+        ),
+      ),
+      'options' => array (
+        'position' => 'acf_after_title',
+        'layout' => 'no_box',
+        'hide_on_screen' => array (
+        ),
+      ),
+      'menu_order' => 0,
+    ));
+  }
